a0b42ae5efbc9e84917e9cb931162352cce70cb9
Joe Bordes <joe@tsolucio.com>
2014-08-12 16:47:45 +0200

    search object in array filter

--- app/js/filters.js
+++ app/js/filters.js
@@ -2,9 +2,21 @@
 
 /* Filters */
 
-angular.module('MarvelJSApp.filters', []).
-  filter('interpolate', ['version', function(version) {
-    return function(text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
-    };
-  }]);
+angular.module('MarvelJSApp.filters', [])
+.filter('getById', function() {
+	return function(input, idvalue, idprop) {
+		if (idprop===undefined) idprop='id';
+		var i = 0, len = input.length;
+		for (; i < len; i++) {
+			if (input[i][idprop] == idvalue) {
+				return input[i];
+			}
+		}
+		return null;
+	}
+})
+.filter('interpolate', [ 'version', function(version) {
+	return function(text) {
+		return String(text).replace(/\%VERSION\%/mg, version);
+	};
+} ]);
