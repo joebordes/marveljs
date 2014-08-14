744d53ec34288ee4ed5a14df33fce58b7d9ad0a8
Joe Bordes <joe@tsolucio.com>
2014-08-15 00:27:47 +0200

    code structure

--- app/js/directives.js
+++ app/js/directives.js
@@ -2,10 +2,11 @@
 
 /* Directives */
 
-
-angular.module('MarvelJSApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+angular.module('MarvelJSApp.directives', [])
+.directive('appVersion',
+[ 'version', function(version) {
+	return function(scope, elm, attrs) {
+		elm.text(version);
+	};
+} ])
+;
